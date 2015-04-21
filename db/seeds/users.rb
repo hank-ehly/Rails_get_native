a28861a8556e6b5bd35b5aeb4ba4c434c9912b6f
Henry Ehly <henry.ehly@gmail.com>
2015-04-21 09:27:05 +0900

    created master seeds.rb and required all seeds/*

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,4 +1,5 @@
 User.create!([
   {first_name: "Henry", last_name: "Ehly", email: "henry.ehly@gmail.com", password_digest: "$2a$10$WjXLx93RphirsB.QXwAIjeQI.i.kB4DqtMkLbzzYvQAw5edrUtKX2"},
-  {first_name: "megutan", last_name: "yamashita", email: "m_yama@email.com", password_digest: "$2a$10$szUzqrzfQgMHHdtckOI0aeCvfZIAzTVUZT8lD2kQHTWr5NWIK7zE2"}
+  {first_name: "megutan", last_name: "yamashita", email: "m_yama@email.com", password_digest: "$2a$10$szUzqrzfQgMHHdtckOI0aeCvfZIAzTVUZT8lD2kQHTWr5NWIK7zE2"},
+  {first_name: "Test", last_name: "User", email: "t_user@email.com", password_digest: "$2a$10$AirtclSKXtYscEczje4nNeVo4V9PreoVhggk2hGGxTZN6rywHGbzy"}
 ])
