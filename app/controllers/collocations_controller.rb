64e41bba6b7440892eba298b9da55e42e91a53e1
Henry Ehly <hank.ehly@mulodo.com>
2015-05-15 09:46:58 +0900

    context well examples

--- app/controllers/collocations_controller.rb
+++ app/controllers/collocations_controller.rb
@@ -10,13 +10,18 @@ class CollocationsController < ApplicationController
     # get coll start position within j_script
     start_pos = video.japanese_script.index(@collocation.collocation)
     # get coll end position within j_scrtipt
+    # end_pos = start_pos + collocation.length
     end_pos = start_pos + @collocation.collocation.size
-      # end_pos = start_pos + collocation.length
     # if start_pos < 10, set start_pos = 10, that way it won't take from the end of the script
-    # elif end_pos > j_script.length, set end_pos = j_script.length so that it doesn't take from beginning
+    if start_pos < 15
+      start_pos = 15
+      # elif end_pos > start_pos (ie it starts at beginning), set end_pos = j_script.length so that it doesn't take from beginning
+    elsif end_pos < start_pos
+      end_pos = video.japanese_script.size
+    end
     # set collocation.context equal to video.j_script[(start_pos - 10 or so)..(end_pos + 10 or so)]
-    # @collocation.context = 
-    @collocation.context = video.japanese_script[(start_pos - 10)..(end_pos + 10)]
+    @collocation.context = video.japanese_script[(start_pos - 15)..(end_pos + 15)]
+    # highlight collocation inside context well
     respond_to do |format|
       if @collocation.save
         message = { success: 'Added collocation successfully.', collocation: @collocation.collocation }
