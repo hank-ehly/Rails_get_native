63d6c44fed23c11caf70de18ffeb328ed13b4894
Henry Ehly <hank.ehly@mulodo.com>
2015-05-21 11:51:43 +0900

    successfully added embededCollocation class to collocations within wells

--- app/assets/javascripts/collocationHighlighting.js
+++ app/assets/javascripts/collocationHighlighting.js
@@ -1,15 +1,44 @@
 $(document).on('page:change', function() {
 	if ($('.collocation_instance')) {
 		$.each($('.collocation_instance'), function(index, val) {
-			 // console.log(val.children[0].innerHTML);
-			 // console.log(val.children[1].innerHTML);
 
-			 var start, end, embededCollocation;
+			var col, well;
 
-			 start = val.children[1].innerHTML.indexOf(val.children[0].innerHTML);
-			 end = start + val.children[0].innerHTML.length;
+			col = val.children[0];
+			well = val.children[1];
 
-			 embededCollocation = val.children[1].innerHTML.substring(start, end);
+
+
+			var start, end, embededCollocation;
+			start = val.children[1].innerHTML.indexOf(val.children[0].innerHTML);
+			end = start + val.children[0].innerHTML.length;
+
+			jQuery.fn.addSpan = function (elName, str, offset, length)
+			{
+			    if(this.innerHTML = str)
+			    {
+			        return this.each(function ()
+			        {
+
+			            this.innerHTML = 
+
+			            this.innerHTML.substring(0,start) +  // up to beginning of col
+
+			            "<span class=\"embededCollocation\">" + 
+
+			            this.innerHTML.substring(start, start+col.innerHTML.length) + // get's collocation
+
+			            "</span>" + 
+
+			            this.innerHTML.substring(end, end+20); // get's end of col
+
+			        });
+			    }
+			};
+
+
+			$(well).addSpan(col.innerHTML, 15, col.length);
+
 
 		});
 	};
